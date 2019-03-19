23ffd93f246741c71ba2ea68229d8b3b0a7b2b82
Fotis Tsakiris <footios76@gmail.com>
2019-03-19 16:23:20 +0200

    338. orders fetched through redux

--- src/store/actions/orderActions.js
+++ src/store/actions/orderActions.js
@@ -37,3 +37,43 @@ export const purchaseInit = () => {
         type: actionTypes.PURCHASE_INIT
     }
 }
+
+export const fetchOrdersSuccess = (orders) => {
+	return {
+		type: actionTypes.FETCH_ORDERS_SUCCES,
+		orders: orders
+	}
+}
+
+export const fetchOrdersFail = (error) => {
+	return {
+		type: actionTypes.FETCH_ORDERS_FAIL,
+		error: error
+	}
+}
+
+export const fetchOrdersStart = () => {
+	return {
+		type: actionTypes.FETCH_ORDERS_START
+	}
+}
+
+export const fetchOrders = () => {
+	return dispatch => {
+		dispatch(fetchOrdersStart())
+		axios
+			.get('/orders.json')
+			.then((res) => {
+				console.log('Orders: ', res.data); // Why is 'data' what we get back from Firebase?
+				//convert the object into an array:
+				const fetchedOrders = [];
+				for (const key in res.data) {
+					if (res.data.hasOwnProperty(key)) {
+						fetchedOrders.push({ ...res.data[key], id: key });
+					}
+				}
+				dispatch(fetchOrdersSuccess(fetchedOrders))
+			})
+			.catch((err) => dispatch(fetchOrdersFail()));
+	}
+}
--- src/store/reducers/orderReducer.js
+++ src/store/reducers/orderReducer.js
@@ -33,6 +33,22 @@ export const orderReducer = (state = initialState, action) => {
             return {
                 ...state,
                 loading: false
+            } 
+        case actionTypes.FETCH_ORDERS_START:
+            return {
+                ...state,
+                loading: true
+            }
+        case actionTypes.FETCH_ORDERS_SUCCES:
+            return {
+                ...state,
+                orders: action.orders,
+                loading: false
+            }
+        case actionTypes.FETCH_ORDERS_FAIL:
+            return {
+                ...state,
+                loading: false
             }
         default:
             return state
