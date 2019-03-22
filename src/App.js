771e3e33d393e891e297319c4d9de8e7eb8a993d
Fotis Tsakiris <footios76@gmail.com>
2019-03-22 22:29:58 +0200

    347*. auth form added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from './containers/Checkout/Checkout'
 import Orders from './containers/Orders/Orders'
+import Auth from './containers/Auth/Auth'
 
 
 class App extends Component {
@@ -17,6 +18,7 @@ class App extends Component {
             {/* The 'exact' in the Route with path='/checkout' was preventing the ContactData to render */}
             <Route path='/checkout' component={Checkout} />
             <Route path='/orders' exact component={Orders} />
+            <Route path='/auth' exact component={Auth} />
             <Route path='/' exact component={BurgerBuilder} />
           </Switch>
         </Layout>
