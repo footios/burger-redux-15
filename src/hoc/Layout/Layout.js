4f8e428fdd02b22326399bd8f4037f355c272645
Fotis Tsakiris <footios76@gmail.com>
2019-03-27 15:49:09 +0200

    367. bug fixes

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,54 +1,55 @@
-import React, { Component } from "react";
-import Eject from "../Eject/Eject";
-import classes from "./Layout.module.css";
-import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
-import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import Eject from '../Eject/Eject';
+import classes from './Layout.module.css';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+import { connect } from 'react-redux';
 
 // We moved Layout to hoc because it wraps the BurgerBuilder
 // in the App comp.
 
 // Child of App
 class Layout extends Component {
-  state = {
-    showSideDrawer: false
-  };
-  componentDidMount() {
-    window.addEventListener("resize", () => {
-      this.setState({ showSideDrawer: window.innerWidth < 500 });
-    });
-  }
+	state = {
+		showSideDrawer: false
+	};
+	componentDidMount() {
+		window.addEventListener('resize', () => {
+			this.setState({ showSideDrawer: window.innerWidth < 500 });
+		});
+	}
 
-  sideDrawerClosedHandler = () => {
-    this.setState({ showSideDrawer: false });
-  };
+	sideDrawerClosedHandler = () => {
+		this.setState({ showSideDrawer: false });
+	};
 
-  sideDrawerToggleHandler = () => {
-    if (window.innerWidth < 500) {
-      this.setState(prevState => {
-        return { showSideDrawer: !prevState.showSideDrawer };
-      });
-    }
-  };
+	sideDrawerToggleHandler = () => {
+		if (window.innerWidth < 500) {
+			this.setState((prevState) => {
+				return { showSideDrawer: !prevState.showSideDrawer };
+			});
+		}
+	};
 
-  render() {
-    return (
-      <Eject>
-        <Toolbar isAuth={this.props.isAuthenticated} DrawerToggleClicked={this.sideDrawerToggleHandler} />
-        <SideDrawer
-          open={this.state.showSideDrawer}
-          closed={this.sideDrawerClosedHandler}
-        />
-        <main className={classes.Content}>{this.props.children}</main>
-      </Eject>
-    );
-  }
+	render() {
+		return (
+			<Eject>
+				<Toolbar isAuth={this.props.isAuthenticated} DrawerToggleClicked={this.sideDrawerToggleHandler} />
+				<SideDrawer
+					isAuth={this.props.isAuthenticated}
+					open={this.state.showSideDrawer}
+					closed={this.sideDrawerClosedHandler}
+				/>
+				<main className={classes.Content}>{this.props.children}</main>
+			</Eject>
+		);
+	}
 }
 
-const mapStateToProps = state => {
-  return {
-    isAuthenticated: state.auth.token !== null
-  }
-}
+const mapStateToProps = (state) => {
+	return {
+		isAuthenticated: state.auth.token !== null
+	};
+};
 
 export default connect(mapStateToProps)(Layout);
