19828df77485a5add40ecab82f13ea3852e58570
Fotis Tsakiris <footios76@gmail.com>
2019-03-28 11:03:47 +0200

    370 . apply updateObject to fetchOrdersReducer

--- src/store/reducers/fetchOrdersReducer.js
+++ src/store/reducers/fetchOrdersReducer.js
@@ -1,28 +1,31 @@
 import * as actionTypes from '../actions/actionTypes';
-import { updateObject } from '../../shared/utility'
+import { updateObject } from '../../shared/utility';
 
 const initialState = {
 	orders: [],
 	loading: true
 };
 
+const fetchOrders = (state, action) =>
+	updateObject(state, {
+		orders: [ ...action.orders ], // concat is not working as expected!!!
+		loading: false
+	});
+
+const fetchOrdersStart = (state, action) => updateObject(state, { loading: true });
+
+const fetchOrdersFail = (state, action) => updateObject(state, { loading: false });
+
+const deleteOrderOnServer = (state, action) => updateObject(state, {});
+
 export const fetchOrdersReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case actionTypes.FETCH_ORDERS:
-			return {
-				orders: [ ...action.orders ], // concat is not working as expected!!!
-				loading: false
-			};
+			return fetchOrders(state, action);
 		case actionTypes.FETCH_ORDERS_START:
-			return {
-				...state,
-				loading: true
-			};
+			return fetchOrdersStart(state, action);
 		case actionTypes.FETCH_ORDERS_FAIL:
-			return {
-				...state,
-				loading: false
-			};
+			return fetchOrdersFail(state, action);
 		case actionTypes.DELETE_ORDER:
 			const orders = state.orders.filter((order) => order.id !== action.orderId);
 			return {
@@ -31,9 +34,7 @@ export const fetchOrdersReducer = (state = initialState, action) => {
 				orders: orders
 			};
 		case actionTypes.DELETE_ORDER_ON_SERVER:
-			return {
-				...state
-			};
+			return deleteOrderOnServer(state, action);
 		default:
 			return state;
 	}
