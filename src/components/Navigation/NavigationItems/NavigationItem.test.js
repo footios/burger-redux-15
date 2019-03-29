884530e31c96df51495d52d94e7237ac0ed6fa85
Fotis Tsakiris <footios76@gmail.com>
2019-03-29 14:07:42 +0200

    380. more tests added to NavigationItems

--- src/components/Navigation/NavigationItems/NavigationItem.test.js
+++ src/components/Navigation/NavigationItems/NavigationItem.test.js
@@ -1,14 +1,23 @@
-import React from 'react'
-import { configure, shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-import NavigationItems from './NavigationItems'
-import NavigationItem from './NavigationItem/NavigationItem'
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem';
 
-configure({adapter: new Adapter()})
+configure({ adapter: new Adapter() });
 describe('<NavigationItems /> ', () => {
-    it('render 2 NavigationItem elems if not auth', () => {
-        const wrapper = shallow(<NavigationItems />)
-        expect(wrapper.find(NavigationItem)).toHaveLength(2)
-    })
-})
+	let wrapper;
+
+	wrapper = shallow(<NavigationItems />);
+    
+	it('render 2 NavigationItem elems if not auth', () => {
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+	});
+    
+	it('render 3 NavigationItem elems if yes auth', () => {
+        // wrapper = shallow(<NavigationItems isAuthenticated />);
+        wrapper.setProps({isAuthenticated: true})
+		expect(wrapper.find(NavigationItem)).toHaveLength(3);
+	});
+});
